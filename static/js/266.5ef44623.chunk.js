51b95d2246cc829d0e8062a6276516d2b9b59aa3
Andrij-Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2023-12-16 12:02:40 +0000

    Deploying to gh-pages from @ Andrij-Panasenko/goit-react-hw-08-phonebook@6e914d71e70578954a8c94815f1a750565bbac68 🚀

---
+++ static/js/266.5ef44623.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,r){r.r(t),r.d(t,{default:function(){return l}});var c=r(4270),n=r(184);function l(){return(0,n.jsxs)("div",{children:[(0,n.jsx)(c.q,{children:(0,n.jsx)("title",{children:"Welcome page \u270c"})}),(0,n.jsx)("h1",{children:"Welcome page \u270c\u270c\u270c\u270c"})]})}}}]);
+//# sourceMappingURL=266.5ef44623.chunk.js.map
